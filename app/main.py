5d20284b9bbbb9d717a76c96d3ad6fc62977704c
lequan <lequan.vn89@gmail.com>
2017-07-20 20:40:05 +0300

    Init commit. Hello World server.

---
+++ app/main.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from aiohttp import web
+
+from app.routes import setup_routes
+
+
+def init(loop):
+    # setup application and extensions
+    app = web.Application(loop=loop)
+
+    # setup views and routes
+    setup_routes(app)
+
+    return app
+
+
+def main():
+    # init logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    loop = asyncio.get_event_loop()
+
+    app = init(loop)
+    web.run_app(app, host='127.0.0.1', port=8080)
